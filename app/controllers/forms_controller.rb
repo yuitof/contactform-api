b4b5930108adfdb7de70fa18d6e65867eec27f4b
Yuito Fujiwara <yuito6187@gmail.com>
2025-09-08 09:26:47 -0700

    implement API key authentication

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,4 +1,8 @@
 class FormsController < ApplicationController
+  TOKEN = "secret"
+
+  before_action :authenticate
+
   def create
     data = form_params().to_h
     FormMailer.with(data: data).confirmation.deliver_later
@@ -7,5 +11,13 @@ class FormsController < ApplicationController
   private
     def form_params
       params.expect(form: [ :message, :email, :firstname, :lastname ])
+    end
+
+    def authenticate
+      authenticate_or_request_with_http_token do |token, options|
+        # Compare the tokens in a time-constant manner, to mitigate
+        # timing attacks.
+        ActiveSupport::SecurityUtils.secure_compare(token, TOKEN)
+      end
     end
 end
